758083a6959b8433625bb064290e62d140dc5760
nevo0309 <nevo19970309@gmail.com.com>
2025-02-07 17:27:04 +0200

    Replace book creation with database retrieval

--- react-template-proj/services/book.service.js
+++ react-template-proj/services/book.service.js
@@ -1,7 +1,9 @@
 import { storageService } from './async-storage.service.js'
 import { utilService } from './util.service.js'
+import { books } from './books.js'
 
 const BOOKS_KEY = 'booksDB'
+let gBooks = []
 _createBooks()
 
 export const bookService = {
@@ -9,10 +11,18 @@ export const bookService = {
   remove,
   save,
   query,
+  getDefaultFilter,
 }
 
-function query() {
+function query(filterBy = {}) {
   return storageService.query(BOOKS_KEY).then((books) => {
+    if (filterBy.title) {
+      const regExp = new RegExp(filterBy.title, 'i')
+      books = books.filter((book) => regExp.test(book.title))
+    }
+    if (filterBy.minPrice) {
+      books = books.filter((book) => book.listPrice.amount >= filterBy.minPrice)
+    }
     return books
   })
 }
@@ -32,35 +42,49 @@ function save(book) {
   }
 }
 
+function getDefaultFilter() {
+  return { title: '', minPrice: '' }
+}
+
 function _createBooks() {
-  let books = utilService.loadFromStorage('BOOKS_KEY')
-  if (!books || !books.length) {
-    books = []
-    books.push(_createBook())
-    books.push(_createBook())
-    books.push(_createBook())
-
-    utilService.saveToStorage(BOOKS_KEY, books)
-  }
+  storageService.query(BOOKS_KEY).then((storedBooks) => {
+    if (!storedBooks || !storedBooks.length) {
+      gBooks = [...books]
+      utilService.saveToStorage(BOOKS_KEY, gBooks)
+    } else {
+      gBooks = storedBooks
+    }
+  })
 }
 
-function _createBook() {
-  const ctgs = ['Love', 'Fiction', 'Poetry', 'Computers', 'Religion']
-  return {
-    id: utilService.makeId(),
-    title: utilService.makeLorem(2),
-    subtitle: utilService.makeLorem(4),
-    authors: [utilService.makeLorem(1)],
-    publishedDate: utilService.getRandomIntInclusive(1950, 2024),
-    description: utilService.makeLorem(20),
-    pageCount: utilService.getRandomIntInclusive(20, 600),
-    categories: [ctgs[utilService.getRandomIntInclusive(0, ctgs.length - 1)]],
-    thumbnail: `http://coding-academy.org/books-photos/${utilService.getRandomIntInclusive(1, 20)}.jpg`,
-    language: 'en',
-    listPrice: {
-      amount: utilService.getRandomIntInclusive(80, 500),
-      currencyCode: 'EUR',
-      isOnSale: Math.random() > 0.7,
-    },
-  }
-}
+// function _createBooks() {
+//   let books = utilService.loadFromStorage('BOOKS_KEY')
+//   if (!books || !books.length) {
+//     books = []
+//     books.push(_createBook())
+//     books.push(_createBook())
+//     books.push(_createBook())
+
+//     utilService.saveToStorage(BOOKS_KEY, books)
+//   }
+// }
+// function _createBook() {
+//   const ctgs = ['Love', 'Fiction', 'Poetry', 'Computers', 'Religion']
+//   return {
+//     id: utilService.makeId(),
+//     title: utilService.makeLorem(2),
+//     subtitle: utilService.makeLorem(4),
+//     authors: [utilService.makeLorem(1)],
+//     publishedDate: utilService.getRandomIntInclusive(1950, 2024),
+//     description: utilService.makeLorem(20),
+//     pageCount: utilService.getRandomIntInclusive(20, 600),
+//     categories: [ctgs[utilService.getRandomIntInclusive(0, ctgs.length - 1)]],
+//     thumbnail: `http://coding-academy.org/books-photos/${utilService.getRandomIntInclusive(1, 20)}.jpg`,
+//     language: 'en',
+//     listPrice: {
+//       amount: utilService.getRandomIntInclusive(80, 500),
+//       currencyCode: 'EUR',
+//       isOnSale: Math.random() > 0.7,
+//     },
+//   }
+// }
