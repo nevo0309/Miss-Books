a1025ed709bb63db3f57282e6a7d00e91ad8f470
nevo0309 <nevo19970309@gmail.com.com>
2025-02-07 17:26:02 +0200

    Add basic filter

--- react-template-proj/pages/BookIndex.jsx
+++ react-template-proj/pages/BookIndex.jsx
@@ -1,19 +1,23 @@
 import { BookList } from '../cmps/BookList.jsx'
 import { bookService } from '../services/book.service.js'
 import { BookDetails } from './BookDetails.jsx'
+import { BookFilter } from '../cmps/BookFilter.jsx'
 
 const { useState, useEffect } = React
 
 export function BookIndex() {
   const [books, setBooks] = useState(null)
+  const [filterBy, setFilterBy] = useState(bookService.getDefaultFilter())
   const [selectedBookId, setSelectedBookId] = useState(null)
 
   useEffect(() => {
     loadBooks()
-  }, [])
+  }, [filterBy])
 
   function loadBooks() {
-    bookService.query().then((books) => {
+    bookService.query(filterBy).then((books) => {
+      console.log(filterBy)
+
       console.log('books', books)
 
       setBooks(books)
@@ -22,6 +26,20 @@ export function BookIndex() {
   function onSetSelectedBookId(bookId) {
     setSelectedBookId(bookId)
   }
+  function onSetFilterBy(filterBy) {
+    setFilterBy({ ...filterBy })
+  }
   if (!books) return 'Loading...'
-  return <section className="book-index">{selectedBookId ? <BookDetails selectedBookId={selectedBookId} /> : <BookList books={books} onSetSelectedBookId={onSetSelectedBookId} />}</section>
+  return (
+    <section className="book-index">
+      {selectedBookId ? (
+        <BookDetails selectedBookId={selectedBookId} />
+      ) : (
+        <React.Fragment>
+          <BookFilter filterBy={filterBy} onSetFilterBy={onSetFilterBy} />
+          <BookList books={books} onSetSelectedBookId={onSetSelectedBookId} />
+        </React.Fragment>
+      )}
+    </section>
+  )
 }
