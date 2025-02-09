07f153193bce6be18866b751ce4c1a1dfd0b0e3b
nevo0309 <nevo19970309@gmail.com.com>
2025-02-09 18:07:26 +0200

    Move 'show more' functionality to component & optimized button rendering

--- react-template-proj/cmps/BookPreview.jsx
+++ react-template-proj/cmps/BookPreview.jsx
@@ -1,13 +1,8 @@
-const { useState } = React
+import { LongTxt } from './LongTxt.jsx'
 
 export function BookPreview({ book }) {
-  const [fullDescription, setFullDescription] = useState(false)
   const originalPrice = book.listPrice.amount
   const discountedPrice = (originalPrice * 0.5).toFixed(2).replace(/\.00$/, '')
-
-  function showMoreDescrip() {
-    setFullDescription((prevState) => !prevState)
-  }
 
   return (
     <section className="book-preview">
@@ -19,12 +14,7 @@ export function BookPreview({ book }) {
       )}
 
       <h4 className="book-title">Title: {book.title}</h4>
-      <p className="book-description">
-        <strong>Description:</strong> {fullDescription ? book.description : `${book.description.substring(0, 100)}...`}
-        <button className="read-more" onClick={showMoreDescrip}>
-          {fullDescription ? ' Show Less' : ' Read More'}
-        </button>
-      </p>
+      <LongTxt book={book} />
 
       {book.listPrice.isOnSale ? (
         <p className="book-price">
