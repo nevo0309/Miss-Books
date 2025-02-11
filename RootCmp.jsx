4411cd4664b098ba80cd3fae385913ce8ee86cf1
nevo0309 <nevo19970309@gmail.com.com>
2025-02-11 18:51:42 +0200

    Add router libs

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,7 +1,13 @@
+const Router = ReactRouterDOM.HashRouter
+const { Routes, Route } = ReactRouterDOM
+
 import { AppHeader } from './cmps/AppHeader.jsx'
 import { About } from './pages/About.jsx'
 import { BookIndex } from './pages/BookIndex.jsx'
 import { Home } from './pages/Home.jsx'
+import { BookIndex } from './pages/BookIndex.jsx'
+import { BookEdit } from './pages/BookEdit.jsx'
+import { UserMsg } from './cmps/UserMsg.jsx'
 
 const { useState } = React
 
